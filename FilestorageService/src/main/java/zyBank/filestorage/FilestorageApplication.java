bde20c6e200aad4caee331ed11d3c96e204d39f4
Little Z <zy_mz17@hotmail.es>
2023-06-07 09:13:03 +0200

    add file

---
+++ FilestorageService/src/main/java/zyBank/filestorage/FilestorageApplication.java
@@ -0,0 +1,25 @@
+package zyBank.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class FilestorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilestorageApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
+
+}
---
+++ FilestorageService/src/main/java/zyBank/filestorage/service/StorageService.java
@@ -0,0 +1,13 @@
+package zyBank.filestorage.service;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface StorageService {
+    void init() throws IOException;
+    String store(MultipartFile file);
+    Resource loadAsResource(String fileName);
+}
